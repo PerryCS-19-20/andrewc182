f05cdc0acb903a18a235ea094469c51e4ee821aa
andrewc182 <jdam115@gmail.com>
2016-12-13 09:03:59 -0700

    Create sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Sitemap</title>
+    <script src="sitemap.js"></script>
+  </head>
+  <body onload="buildMap('sitemapWrapper');">
+    <div id="sitemapWrapper"></div>
+    <ul>
+    </ul>
+    <script>
+      function buildMap() {
+        var 
+    </script>
+  </body>
+</html>
